44498a8201ac8e327f0c7eddd56c8890e44d47ee
ZixuanGong <15026970772@163.com>
2014-09-14 14:43:33 -0400

    finish path +

--- shell/shell.c
+++ shell/shell.c
@@ -5,47 +5,37 @@
 #define TRUE 1
 
 // char *command[] = {"cd","exit"};
-char *cmdArgv[5];
+char *cmdArgv[5]={NULL, NULL, NULL, NULL, NULL};
 int cmdArgc = 0;
 char *cmd = NULL;
+char *path;
+int path_len = 0;
 
-void read_command() {
-	char line[256]; //user input
-	char *token;
-	
-	fgets(line, sizeof(line), stdin);
-
-	//split line
-	token = strtok(line, " ");
-	if(token == NULL) //no white space
-		cmdArgv[0] = line;
-
-	while(token != NULL){
-		cmdArgv[cmdArgc] = token;
-		token = strtok(NULL, " ");
-		cmdArgc++;
-	}
-	cmd = cmdArgv[0];
-	printf("%s\n", cmd);
-}
 
 void type_prompt(){
 	printf("$ ");
 }
+void init(){
+	char *init_path = "/bin";
+	path_len = strlen(init_path);
+	path = (char*)malloc(strlen(init_path) * sizeof(char));
+	strcpy(path, init_path);
+	printf("%d\n", path_len);
+}
 
 int main(int argc, char **argv) {
 	// char *command[]={"exit"};
-	char *cmdArgv[5];
-	int cmdArgc = 0;
 	char line[256]; //user input
 	char *token;
-	char *cmd;
+	int status = -1;
+	// int i;
+
+	init();
 
 	while (TRUE) {
 		type_prompt();
 
 		//read input
-		fflush(stdout);
 		fgets(line, sizeof(line), stdin);
 		//cancel the last \n
 		char *p = NULL;
@@ -65,8 +55,42 @@ int main(int argc, char **argv) {
 		cmd = cmdArgv[0];
 
 		//exit
-		if(strcmp(cmd, "exit") == 0)
+		if(!strcmp(cmd, "exit"))
 			exit(0);
+
+		//path
+		else if(!strcmp(cmd, "path")){
+			//null pointer, print path
+			if(!cmdArgv[1]) {
+				printf("%s\n", path);
+			//+ - path
+			} else if(!strcmp(cmdArgv[1], "+")){
+				//reallocate memory
+				path_len += strlen(cmdArgv[2]) + 1; //1 for colon
+				path = (char*)realloc(path, path_len * sizeof(char));
+				strcat(path, ":");
+				strcat(path, cmdArgv[2]);
+				printf("%s\n", path);
+				printf("%d\n", path_len);
+			} else if(!strcmp(cmdArgv[1], "-")){
+
+			}
+		}
+
+		//ls
+		else if(strcmp(cmd, "ls") == 0){
+			printf("reach ls");
+			if(fork() != 0){
+				/* Parent */
+				wait(&status);
+				printf("\n");
+
+			} else{
+				/* Child*/
+				// execv(file_name, cmdArgv);
+				exit(0);
+			}
+		}
 
 		// printf("%d\n", t);
 		// printf("%s\n", cmd);
