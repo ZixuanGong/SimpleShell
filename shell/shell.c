486e7e0e5a7fb0fb6fd9a92f395c0c14883fe41b
Zixuan Gong <zg2203@columbia.edu>
2014-09-16 00:24:25 -0400

    final version

--- shell/shell.c
+++ shell/shell.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #define TRUE 1
 #define OFFSET 10
+#define MAX_CMD_ARGS 10
 
 char **path;
 size_t path_num = 0;
@@ -13,9 +14,6 @@ size_t single_path_len =30;
 
 char **cmdArgv;
 size_t cmdArgc = 0;
-size_t cmdArgSize = 10;
-
-int status = -1;
 
 void print_error(char *msg)
 {
@@ -27,31 +25,32 @@ void print_error(char *msg)
 
 void init()
 {
+	int i;
 	errno = 0;
 
 	path = malloc(path_size * sizeof(char*));
-	int i;
 	for (i = 0; i < path_size; i++)
 		path[i] = malloc(single_path_len * sizeof(char));
 	strcpy(path[0], "/bin");
 	path_num++;
-
 }
 
 void read_and_parse_input(){
-	char *line;
+	char *line = NULL;
 	size_t n = 128;
+	char *tail = NULL;
+	char *token = NULL;
+
 	if (getline(&line, &n, stdin) == -1) {
-		print_error("Error in getline()");
+		print_error("error in getline()");
 		return;
 	}
 	/* cancel the last \n */
-	char *tail = NULL;
 	if ((tail = strchr(line, '\n')) != NULL)
 		*tail = '\0';
 	/* split input */
-	char *token = strtok(line, " ");
-	while (token != NULL && cmdArgc <= 10) {
+	token = strtok(line, " ");
+	while (token != NULL && cmdArgc <= MAX_CMD_ARGS) {
 		cmdArgv[cmdArgc] = token;
 		token = strtok(NULL, " ");
 		cmdArgc++;
@@ -71,13 +70,13 @@ int find_position(char **array, size_t num, char *string)
 
 void enlarge_path_size()
 {
+	int i;
 	char **bigger_path = realloc(path, path_size + OFFSET);
 	if (bigger_path == NULL) {
-		print_error("Error in enlarge_path_size()");
+		print_error("error in enlarge_path_size()");
 	}
 	else {
 		path = bigger_path;
-		int i;
 		for (i = path_size; i < path_size + OFFSET; i++)
 			path[i] = malloc(single_path_len * sizeof(char));
 		path_size += OFFSET;
@@ -87,11 +86,12 @@ void enlarge_path_size()
 void manage_path()
 {
 	char *dir = cmdArgv[2];
+	int i = 0;
+	int pos;
 
 	/* path */
 	if (cmdArgc == 1) {
 		printf("%s", path[0]);
-		int i = 0;
 		for(i = 1; i < path_num; i++)
 			printf(":%s", path[i]);
 		printf("\n");
@@ -99,7 +99,7 @@ void manage_path()
 	/* path + */ 
 	else if (strcmp(cmdArgv[1], "+") == 0 && dir != NULL) {
 		if (find_position(path, path_num, dir) >= 0) {
-			print_error("Directory already exists");
+			print_error("directory already exists");
 			return;
 		}
 		if (path_num >= path_size) 
@@ -110,14 +110,14 @@ void manage_path()
 	/* path - */ 
 	else if (strcmp(cmdArgv[1], "-") == 0 && dir != NULL) {
 		if(path_num == 0){
-			print_error("The path is empty!");
+			print_error("the path is empty");
 			return;
 		}
 
-		int pos = find_position(path, path_num, dir);
+		pos = find_position(path, path_num, dir);
 
 		if (pos == -1) {
-			print_error("No such directory in the path!");
+			print_error("no such directory in the path");
 			return;
 		}
 
@@ -129,19 +129,23 @@ void manage_path()
 		}
 		path_num--;
 	} else {
-		print_error("Invalid command");
+		print_error("invalid command");
 	}
 }
 
 void change_directory(){
 	char *file_name = malloc(128 * sizeof(char));
-	strcpy(file_name, cmdArgv[1]);
+	if (cmdArgv[1] != NULL)
+		strcpy(file_name, cmdArgv[1]);
+	else
+		file_name = "/home";
 	if (chdir(file_name) == -1)
 		print_error(NULL);
 }
 
 void exec_command()
 {
+	int status;
 	char *file_name = malloc(128 * sizeof(char));
 	strcpy(file_name, "/bin/");
 	strcat(file_name, cmdArgv[0]);
@@ -150,26 +154,30 @@ void exec_command()
 		wait(&status);
 
 	} else{
-		execv(file_name, cmdArgv);
-		exit(0);
+		if (execv(file_name, cmdArgv) == -1) {
+			print_error("command not found");
+			_exit(EXIT_FAILURE);
+		}
 	}
 }
 
 int main(int argc, char **argv)
 {
+	int i;
+	int pos;
 	char *commands[] = {"cd", "exit", "path"};
-	cmdArgv = malloc(cmdArgSize * sizeof(char*));;
+	cmdArgv = malloc(MAX_CMD_ARGS * sizeof(char*));;
 	init();
 
 	while (TRUE) {
-		cmdArgc = 0;
-		int i;
-		for (i = 0; i < cmdArgSize; i++)
+		for (i = 0; i < MAX_CMD_ARGS; i++)
 			cmdArgv[i] = NULL;
+		cmdArgc = 0;
 
 		printf("$ ");
 		read_and_parse_input();
-		switch (find_position(commands, 3, cmdArgv[0])) {
+		pos = find_position(commands, 3, cmdArgv[0]);
+		switch (pos) {
 		case 0:
 			change_directory();
 			break;
